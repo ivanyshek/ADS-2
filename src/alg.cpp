d27e0215bbf61576da7ba712f4a1f116160aff93
ivanyshek <105326502+ivanyshek@users.noreply.github.com>
2024-03-09 15:24:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,54 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double result = 1;
+    if (n == 0) {
+        return 1;
+    }
+    for (int i = 0; i < n; i++) {
+        result = result * value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n == 1) {
+        return 1;
+    } else {
+        return n * fact(n - 1);
+    }
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    if (count == 0) {
+        return 1;
+    } else if (count > 0) {
+        return calcItem(x, count) + expn(x, count - 1);
+    } else {
+        return 0;
+    }
 }
 
 double sinn(double x, uint16_t count) {
-
+    if (count == 1) {
+        return x;
+    } else if (count > 1) {
+        return pown(-1, count - 1) * calcItem(x, 2 * count - 1) + sinn(x, count - 1);
+    } else {
+        return 0;
+    }
 }
 
 double cosn(double x, uint16_t count) {
-
+    if (count == 1) {
+        return 1;
+    } else if (count > 1) {
+        return pown(-1, count - 1) * calcItem(x, 2 * count - 2) + cosn(x, count - 1);
+    } else {
+        return 0;
+    }
 }
